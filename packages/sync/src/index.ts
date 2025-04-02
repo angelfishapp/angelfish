f93dea072b60f899f19a680ace1b5f1ba4d55dad
David Gildeh <dgildeh@davidgildeh.com>
2025-04-02 12:16:50 -0700

    Improved test config for cloudservice tests

--- packages/sync/src/index.ts
+++ packages/sync/src/index.ts
@@ -16,4 +16,7 @@ window.onload = async () => {
 
   // Initialize the AuthService
   await AuthService.init()
+  // Wait a few seconds on startup and start the sync process
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+  await SyncService.sync()
 }
