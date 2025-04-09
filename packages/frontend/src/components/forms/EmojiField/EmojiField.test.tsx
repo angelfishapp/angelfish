3a4ce4c0c27c34560ec82e8f316a89fdbf3fc77a
ahmedjw <ahmedvander@gmail.com>
2025-04-09 15:25:06 +0200

    initial file for emoji tst file not solved yet

---
+++ packages/frontend/src/components/forms/EmojiField/EmojiField.test.tsx
@@ -0,0 +1,36 @@
+// ColorField.test.tsx
+import theme from '@/app/theme'
+import { ThemeProvider } from '@emotion/react'
+import { composeStories } from '@storybook/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as stories from './EmojiField.stories'
+
+const { Default } = composeStories(stories)
+
+describe('EmojiFIeld', () => {
+  it('opens EmojiFIeld and triggers onChange when EMoji is selected', async () => {
+    const user = userEvent.setup()
+    render(
+      <ThemeProvider theme={theme}>
+        <Default />
+      </ThemeProvider>,
+    )
+
+    const emojiButton = screen.getByRole('button')
+    expect(emojiButton).toBeInTheDocument()
+    // await user.click(emojiButton)
+
+    // // const picker = await document.querySelector('.sketch-picker')
+    // // expect(picker).toBeInTheDocument()
+    // // expect(picker).toBeVisible()
+    // // const emoji = screen.getByLabelText('😀') 
+    // // expect(emoji).toBeInTheDocument()
+    // // expect(emoji).toBeVisible()
+
+    // // await user.clear(inputById!)
+    // // await user.type(inputById!, '#ff0000')
+
+    // // expect((inputById as HTMLInputElement).value).toBe('#ff0000')
+  })
+})
