64bbd4bc6609d7c709277ee87a528223615d37e4
ahmedjw <ahmedvander@gmail.com>
2025-04-09 13:23:00 +0200

    COuntryField test file

---
+++ packages/frontend/src/components/forms/CountryField/CountryField.test.tsx
@@ -0,0 +1,30 @@
+import theme from '@/app/theme'
+import { ThemeProvider } from '@mui/system'
+import { composeStories } from '@storybook/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import * as stories from './CountryField.stories'
+
+const { Default } = composeStories(stories)
+
+describe('renders CountryFIeld Story', () => {
+  test('render of default Story', async () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Default />
+      </ThemeProvider>,
+    )
+
+    const [title] = screen.getAllByText(/Country Field/i)
+    expect(title).toBeInTheDocument()
+
+    const input = screen.getByPlaceholderText('Search Countries...') as HTMLInputElement
+    expect(input).toBeInTheDocument()
+
+    const helperText = screen.getByText(/Select a country from the list/i)
+    expect(helperText).toBeInTheDocument()
+
+    fireEvent.change(input, { target: { value: 'canada' } })
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
+    expect(input.value).toBe('Canada')
+  })
+})
