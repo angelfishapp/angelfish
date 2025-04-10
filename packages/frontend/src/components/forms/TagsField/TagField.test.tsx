0425d1b9c3c1697a297d5effe715f496031368a6
ahmedjw <ahmedvander@gmail.com>
2025-04-10 14:44:46 +0200

    TagsField test file

---
+++ packages/frontend/src/components/forms/TagsField/TagField.test.tsx
@@ -0,0 +1,32 @@
+import theme from '@/app/theme'
+import { ThemeProvider } from '@mui/material/styles'
+import { composeStories } from '@storybook/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import React from 'react'
+import { describe, expect, it } from 'vitest'
+import * as stories from './TagsField.stories'
+
+const { Default } = composeStories(stories)
+
+describe('TagField tests', () => {
+  const renderWithTheme = (ui: React.ReactElement) =>
+    render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>)
+
+  it('renders without crashing', () => {
+    renderWithTheme(<Default />)
+    const [title] = screen.getAllByText('Tags Field')
+    expect(title).toBeInTheDocument()
+    const inputField = screen.getByRole('combobox')
+    expect(inputField).toBeInTheDocument()
+  })
+
+  it('adds a tag when input is submitted', () => {
+    const handleChange = vi.fn()
+    renderWithTheme(<Default onChange={handleChange} />)
+    const inputField = screen.getByRole('combobox')
+    fireEvent.change(inputField, { target: { value: 'Tag 1' } })
+    fireEvent.keyDown(inputField, { key: 'Enter', code: 'Enter' })
+    const addedTag = screen.getByText('Tag 1')
+    expect(addedTag).toBeInTheDocument()
+  })
+})
