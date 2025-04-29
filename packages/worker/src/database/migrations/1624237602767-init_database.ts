465394e344df466903aafc5858c1f80f6f5d3bbf
David Gildeh <dgildeh@davidgildeh.com>
2025-04-28 21:12:28 -0700

    Added index to speed up transaction list queries

--- packages/worker/src/database/migrations/1624237602767-init_database.ts
+++ packages/worker/src/database/migrations/1624237602767-init_database.ts
@@ -196,9 +196,14 @@ export default class InitDatabase1624237602767 implements MigrationInterface {
                             FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
                         );`
     await queryRunner.query(initSql)
+
+    // Create indexes
     initSql = `
                         CREATE INDEX IDX_line_items_tags ON line_item_tags (
                             "line_item_id"
+                        );
+                        CREATE INDEX IDX_transaction_line_items ON line_items (
+                            "transaction_id"
                         );`
     await queryRunner.query(initSql)
 
